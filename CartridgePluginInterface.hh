ffd66569e3db9a5ad95a9fc4caa6b98a73113eef
Rogerio Matte Machado <rogermm@gmail.com>
2016-07-31 19:59:39 -0300

    Initial implementation

---
+++ CartridgePluginInterface.hh
@@ -0,0 +1,83 @@
+#ifndef CARTRIDGEPLUGININTERFACE_HH
+#define CARTRIDGEPLUGININTERFACE_HH
+
+#include <stdint.h>
+
+#define OPCODE_LDA_MMMM  (uint32_t(1 << 0))
+#define OPCODE_POP_AF    (uint32_t(1 << 1))
+#define OPCODE_LDHL_MMMM (uint32_t(1 << 2))
+
+#if defined(__linux__)
+#define OPENMSXEXPORT
+#else
+#define OPENMSXEXPORT __declspec(dllexport)
+#endif
+
+namespace plugin
+{
+   static const long MAJOR = 0; ///< Interface major version
+   static const long MINOR = 1; ///< Interface minor version
+
+   class CartridgePluginInterface;
+
+   typedef OPENMSXEXPORT uint32_t(*tOpenMsxPluginEntry) (CartridgePluginInterface *, long major, long minor);
+
+   class CartridgePluginEventInterface;
+
+   class CartridgePluginInterface
+   {
+   public:
+      virtual void RegisterEvents(CartridgePluginEventInterface *) = 0;
+      virtual void pluginInvalidateMemCache(uint16_t start, unsigned size) = 0;
+      virtual void pluginPrintWarning(const char *msg, ...) = 0;
+      virtual void pluginPrintError(const char *msg, ...) = 0;
+      virtual void pluginPrintProgress(const char *msg, ...) = 0;
+      virtual void pluginPrintInfo(const char *msg, ...) = 0;
+      virtual void pluginReset(void) = 0;
+      virtual void pluginPowerDown(void) = 0;
+      virtual void pluginPowerUp(void) = 0;
+      virtual bool pluginExecute(const char *) = 0;
+      virtual void pluginGetConfigValue(const char *name, char *value, int size) = 0;
+      virtual void pluginRegisterGlobalRead(uint16_t mask, uint16_t address, uint32_t opcodes) = 0;
+      virtual void pluginUnregisterGlobalRead(uint16_t mask, uint16_t address) = 0;
+      virtual void pluginRegisterGlobalWrite(uint16_t address) = 0;
+      virtual void pluginUnregisterGlobalWrite(uint16_t address) = 0;
+      virtual void pluginRegisterIoGlobalRead(uint8_t port) = 0;
+      virtual void pluginUnregisterIoGlobalRead(uint8_t port) = 0;
+      virtual void pluginRegisterIoGlobalWrite(uint8_t port) = 0;
+      virtual void pluginUnregisterIoGlobalWrite(uint8_t port) = 0;
+      virtual bool pluginIsM1Cycle(uint16_t address) = 0;
+      virtual int  pluginGetCacheLineSize(void) = 0;
+      virtual uint16_t pluginGetPC(void) = 0;
+   };
+
+   class CartridgePluginEventInterface
+   {
+   public:
+      virtual void     OnOpenMsxPowerUp(uint64_t /*time*/) {};
+      virtual void     OnOpenMsxPowerDown(uint64_t /*time*/) {};
+      virtual void     OnOpenMsxReset(uint64_t /*time*/) {};
+      virtual void     OnOpenMsxIorqM1(uint64_t /*time*/) {};
+
+      virtual uint8_t  OnOpenMsxReadMem(uint16_t /*address*/, uint16_t /*pc*/, uint64_t /*time*/) { return 0; };
+      virtual void     OnOpenMsxWriteMem(uint16_t /*address*/, uint8_t /*value*/, uint16_t /*pc*/, uint64_t /*time*/) {};
+      virtual uint8_t  OnOpenMsxPeekMem(uint16_t /*address*/) { return 0; };
+      virtual const uint8_t* OnOpenMsxGetReadCacheLine(uint16_t /*start*/) const { return nullptr; };
+
+      virtual void     OnOpenMsxWriteIO(uint16_t /*port*/, uint8_t /*value*/, uint16_t /*pc*/, uint16_t /*time*/) {};
+      virtual uint8_t  OnOpenMsxReadIO(uint16_t /*port*/, uint16_t /*pc*/, uint16_t /*time*/) { return 0xff; };
+      virtual uint8_t  OnOpenMsxPeekIO(uint16_t /*port*/) { return 0xff; };
+
+      virtual void     OnOpenMsxGlobalRead(uint16_t /*address*/, uint8_t /*value*/, uint16_t /*pc*/, uint64_t /*time*/) {};
+      virtual void     OnOpenMsxGlobalWrite(uint16_t /*address*/, uint8_t /*value*/, uint16_t /*pc*/, uint64_t /*time*/) {};
+      virtual void     OnOpenMsxGlobalInpRead(uint16_t /*address*/, uint8_t /*value*/, uint16_t /*pc*/, uint64_t /*time*/) {};
+      virtual void     OnOpenMsxGlobalOutpWrite(uint16_t /*address*/, uint8_t /*value*/, uint16_t /*pc*/, uint64_t /*time*/) {};
+      virtual uint8_t *OnOpenMsxGetWriteCacheLine(uint16_t /*start*/) const { return nullptr; };
+
+      virtual bool     OnOpenMsxPluginLoad(void) { return false; };
+      virtual bool     OnOpenMsxPluginUnLoad(void) { return false; };
+   };
+
+}; //  namespace plugin
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,275 @@
+/*
+ * 16-bits MSX mapper
+ * See http://map.grauw.nl/resources/msx_io_ports.php search "16-bit mappers"
+ */
+#include "CartridgePluginInterface.hh"
+
+#define MBYTE (1024 * 1024)
+
+/**
+ * 16-bits mapper size
+ */
+#define MAPPER_SIZE (16*MBYTE)
+
+#define MAPPER_SIZE_MASK (MAPPER_SIZE-1)
+
+#define MAPPER_CTRL_4 0xf8 ///< Memory Mapper segment selection MSB in 16-bit mappers
+#define MAPPER_CTRL_5 0xf9 ///< Memory Mapper segment selection MSB in 16-bit mappers
+#define MAPPER_CTRL_6 0xfa ///< Memory Mapper segment selection MSB in 16-bit mappers
+#define MAPPER_CTRL_7 0xfb ///< Memory Mapper segment selection MSB in 16-bit mappers
+#define MAPPER_CTRL_0 0xfc ///< Memory Mapper segment selection LSB
+#define MAPPER_CTRL_1 0xfd ///< Memory Mapper segment selection LSB
+#define MAPPER_CTRL_2 0xfe ///< Memory Mapper segment selection LSB
+#define MAPPER_CTRL_3 0xff ///< Memory Mapper segment selection LSB
+
+/**
+ * 16 bits value
+ */
+typedef union 
+{
+   struct 
+   {
+      uint8_t l; ///< 8 bits low byte
+      uint8_t h; ///< 8 bits high byte
+   } B;
+   uint16_t W; ///< 16 bits value 
+} ValuePair;
+
+/**
+ * MSX 16-bit memory mapper
+ */
+uint8_t ram[MAPPER_SIZE];
+
+/**
+ * 16 bits mapper setting on Z80 page 0,1,2,3
+ */
+ValuePair map[4];
+
+using namespace plugin;
+
+CartridgePluginInterface *cpi;
+
+class OpenMsxPluginEvent;
+OpenMsxPluginEvent *evt;
+
+
+/**
+ * OPENMSX event handler
+ */
+class OpenMsxPluginEvent : public CartridgePluginEventInterface
+{
+   /**
+    * Power up event
+    */
+   void OnOpenMsxPowerUp(uint64_t time) override
+   {
+      cpi->pluginPrintInfo("Power up");
+   };
+
+   /**
+    * Power down event
+    */
+   void OnOpenMsxPowerDown(uint64_t time) override
+   {
+      cpi->pluginPrintInfo("Power down");
+   };
+
+   /**
+    * Reset event
+    */
+   void OnOpenMsxReset(uint64_t time) override
+   {
+      cpi->pluginPrintInfo("Reset");
+   };
+
+   /**
+    * Z80 IRQ\ = 0 M1\ = 0
+    */
+   void OnOpenMsxIorqM1(uint64_t time) override
+   {
+      // None
+   };
+
+   /**
+    *
+    */
+   inline uint32_t getIdx(uint16_t address)
+   {
+      //             Mapper segment
+      //      |-------------------------|
+      //      |      Z80 page           |      mapper offset (max 16KB)
+      //      |    |-----------|        |    |----------------|
+      //      v    v           v        v    v                v    
+      return ((map[address >> 14].W << 14) | (address & 0x3fff)) & MAPPER_SIZE_MASK;
+   }
+
+   /**
+    * Read memory event
+    * TODO improve performance. See "OnOpenMsxGetReadCacheLine"
+    */
+   uint8_t OnOpenMsxReadMem(uint16_t address, uint16_t pc, uint64_t time) override
+   { 
+      return ram[getIdx(address)];
+   };
+
+   /**
+    * Write memory event
+    * TODO improve performance. See "OnOpenMsxGetWriteCacheLine"
+    */
+   void OnOpenMsxWriteMem(uint16_t address, uint8_t value, uint16_t pc, uint64_t time) override
+   {
+      ram[getIdx(address)] = value;
+   };
+
+   /**
+    * Peek memory event
+    */
+   uint8_t OnOpenMsxPeekMem(uint16_t address) override 
+   { 
+      return OnOpenMsxReadMem(address, 0, 0);
+   };
+
+   /**
+    * 
+    */
+   const uint8_t *OnOpenMsxGetReadCacheLine(uint16_t start) const override
+   { 
+      return nullptr; 
+   };
+
+   /**
+    *
+    */
+   uint8_t *OnOpenMsxGetWriteCacheLine(uint16_t start) const override
+   {
+      return nullptr;
+   };
+
+   /**
+    * Z80 output port
+    */
+   void OnOpenMsxWriteIO(uint16_t port, uint8_t value, uint16_t pc, uint16_t time) override
+   {
+   };
+
+   /**
+    * Z80 input port
+    */
+   uint8_t OnOpenMsxReadIO(uint16_t port, uint16_t pc, uint16_t time)
+   { 
+      switch (port & 0xff)
+      {
+         // Note that reading these registers is not reliable, and should not be done.
+         //   |---> Z80 port
+         //   |                         |--> Z80 page
+         //   v                         v
+         case MAPPER_CTRL_7: return map[3].B.h; // MSB Mapper segment for page 3 (#C000-#FFFF)
+         case MAPPER_CTRL_6: return map[2].B.h; // MSB Mapper segment for page 2 (#8000-#BFFF)
+         case MAPPER_CTRL_5: return map[1].B.h; // MSB Mapper segment for page 1 (#4000-#7FFF)
+         case MAPPER_CTRL_4: return map[0].B.h; // MSB Mapper segment for page 0 (#0000-#3FFF)
+         case MAPPER_CTRL_3: return map[3].B.l; // LSB Mapper segment for page 3 (#C000-#FFFF)
+         case MAPPER_CTRL_2: return map[2].B.l; // LSB Mapper segment for page 2 (#8000-#BFFF)
+         case MAPPER_CTRL_1: return map[1].B.l; // LSB Mapper segment for page 1 (#4000-#7FFF)
+         case MAPPER_CTRL_0: return map[0].B.l; // LSB Mapper segment for page 0 (#0000-#3FFF)
+      }
+
+      return 0xff; 
+   };
+
+   /**
+    * Peek memory
+    */
+   uint8_t OnOpenMsxPeekIO(uint16_t port) override
+   { 
+      return 0xff; 
+   };
+
+   /**
+    * Global read memory
+    */
+   void OnOpenMsxGlobalRead(uint16_t address, uint8_t value, uint16_t pc, uint64_t time) override
+   {
+      // None
+   };
+
+   /**
+    * Global write memory
+    */
+   void OnOpenMsxGlobalWrite(uint16_t address, uint8_t value, uint16_t pc, uint64_t time) override
+   {
+   };
+
+   /**
+    * Global IO read
+    */
+   void OnOpenMsxGlobalInpRead(uint16_t address, uint8_t value, uint16_t pc, uint64_t time) override
+   {
+      // None
+   };
+
+   /**
+    * Global IO write
+    */
+   void OnOpenMsxGlobalOutpWrite(uint16_t port, uint8_t value, uint16_t pc, uint64_t time) override
+   {
+      switch (port & 0xff)
+      {
+      //   |---> Z80 port
+      //   |                  |--> Z80 page
+      //   v                  v
+      case MAPPER_CTRL_7: map[3].B.h = value; break; // Memory Mapper segment selection MSB in 16 bit mappers
+      case MAPPER_CTRL_6: map[2].B.h = value; break; // Memory Mapper segment selection MSB in 16 bit mappers
+      case MAPPER_CTRL_5: map[1].B.h = value; break; // Memory Mapper segment selection MSB in 16 bit mappers
+      case MAPPER_CTRL_4: map[0].B.h = value; break; // Memory Mapper segment selection MSB in 16 bit mappers
+      case MAPPER_CTRL_3: map[3].B.l = value; break; // Memory Mapper segment selection LSB in 16 bit mappers
+      case MAPPER_CTRL_2: map[2].B.l = value; break; // Memory Mapper segment selection LSB in 16 bit mappers
+      case MAPPER_CTRL_1: map[1].B.l = value; break; // Memory Mapper segment selection LSB in 16 bit mappers
+      case MAPPER_CTRL_0: map[0].B.l = value; break; // Memory Mapper segment selection LSB in 16 bit mappers
+      }
+   };
+
+   /**
+    * Load plugin event
+    */
+   bool OnOpenMsxPluginLoad(void) override 
+   { 
+      cpi->pluginRegisterIoGlobalWrite(MAPPER_CTRL_7);
+      cpi->pluginRegisterIoGlobalWrite(MAPPER_CTRL_6);
+      cpi->pluginRegisterIoGlobalWrite(MAPPER_CTRL_5);
+      cpi->pluginRegisterIoGlobalWrite(MAPPER_CTRL_4);
+      cpi->pluginRegisterIoGlobalWrite(MAPPER_CTRL_3);
+      cpi->pluginRegisterIoGlobalWrite(MAPPER_CTRL_2);
+      cpi->pluginRegisterIoGlobalWrite(MAPPER_CTRL_1);
+      cpi->pluginRegisterIoGlobalWrite(MAPPER_CTRL_0);
+
+      return true; // Success loading plugin
+   }
+
+   /**
+    * Unload plugin event
+    */
+   bool OnOpenMsxPluginUnLoad(void) override
+   { 
+      return true; // Success unloading plugin
+   };
+};
+
+/**
+ * DLL plugin entry point
+ */
+extern "C" uint32_t OPENMSXEXPORT OpenMsxPluginEntry(
+           CartridgePluginInterface *cpi_, long major, long minor)
+{
+   if(major == plugin::MAJOR)
+   {
+      cpi = cpi_;
+
+      // Create event handler
+      evt = new OpenMsxPluginEvent();
+
+      // Register plugin events
+      cpi->RegisterEvents(evt);
+   }
+
+   return (major == plugin::MAJOR);
+}
